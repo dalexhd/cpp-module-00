13f58cfb6bff33e203167e89ea8116152a7c548a
DalexHD <eldalexhd@gmail.com>
2021-10-12 18:40:59 +0000

    feat: add first ex.

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -1,6 +1,34 @@
-#include <iostream>
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aborboll <aborboll@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/10/12 18:40:04 by aborboll          #+#    #+#             */
+/*   Updated: 2021/10/12 18:40:07 by aborboll         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-int main() {
-    std::cout << "Hello World!";
-    return 0;
+
+#include <iostream>
+#include <string.h>
+
+int main(int argc, char **argv)
+{
+	if (argc == 1)
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *\n";
+	else
+	{
+		for (int u = 1; u < argc; ++u)
+		{
+			std::string str(argv[u]);
+			for (size_t i = 0; i < str.length(); i++)
+				std::cout << (char)std::toupper(str[i]);
+			if (u + 1 < argc)
+				std::cout << " ";
+		}
+		std::cout << "\n";
+	}
+	return (0);
 }
